67e74e6baf74e038f2f2360deb40941b641000d0
Justyn Pham <jpham@extron.com>
2018-07-10 17:35:28 -0700

    add pattern/concentric_circles

---
+++ src/patterns/concentric_circles.js
@@ -0,0 +1,39 @@
+
+import { NUM_LEDS_PER_STRIP } from '../canopy';
+import { RGB } from '../colors';
+
+
+
+export class ConcentricCircles {
+    constructor(params) {
+        this.circles = [];
+        let step = Math.floor(NUM_LEDS_PER_STRIP / params.Qty);
+        let i = 1;
+        while (i <= params.Qty) {
+            this.circles.push(step * i);
+            i++;
+        }
+        this.color = params.Color;
+        this.width = params.Width;
+        this.growOut = params.GrowOut;
+        this.velocity = params.Velocity;
+        this.offset = 0;
+    }
+
+    update () {
+        this.offset += this.growOut ? this.velocity : -this.velocity;
+        if (this.offset <= 0) this.offset = NUM_LEDS_PER_STRIP;
+    }
+
+    render (canopy) {
+        this.circles.forEach((circle) => {
+            canopy.strips.forEach((strip) => {
+                let i = 0;
+                while (i < this.width) {
+                    strip.updateColor((circle + this.offset + i) % NUM_LEDS_PER_STRIP, this.color);
+                    i++;
+                }
+            });
+        });
+    }
+}
