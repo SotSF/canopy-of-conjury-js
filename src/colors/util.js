f535f130d28c74bb2366f74b8fa35103d4e1bb12
Milo Toor <mtoor@omicia.com>
2018-07-10 22:37:08 -0700

    Create `ColorPicker` component

--- src/colors/util.js
+++ src/colors/util.js
@@ -6,3 +6,20 @@ export const rgbToHex = ({ r, g, b }) => (
 );
 
 
+/** Given an integer in the range [0, 255], this will return the hex string */
+const convertIntToHex = (int) => {
+    const hexAlphabet = '0123456789ABCDEF';
+    const quotient = Math.floor(int / 16);
+    const remainder = int % 16;
+
+    return `${hexAlphabet[quotient]}${hexAlphabet[remainder]}`;
+};
+
+/** Given an RGB object, this will return the corresponding hex triplet */
+export const rgbToHexString = ({ r, g, b }) => {
+    const rHex = convertIntToHex(r);
+    const gHex = convertIntToHex(g);
+    const bHex = convertIntToHex(b);
+
+	return `#${rHex}${gHex}${bHex}`;
+};
