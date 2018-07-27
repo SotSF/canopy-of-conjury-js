dcf6d3a05744824f6e6caa308775fbc51ec04f35
Milo Toor <mtoor@omicia.com>
2018-07-27 02:22:27 -0700

    Use typescript for color functions, add utilities

---
+++ src/colors/index.ts
@@ -0,0 +1,53 @@
+
+import { hsvToRgb, rgbToHex } from './util';
+
+
+export * from './util';
+
+export class RGB {
+    r = null;
+    g = null;
+    b = null;
+
+    constructor (r, g, b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    /**
+     * Converts the color to hexadecimal triplet notation, which is ThreeJS's preferred
+     * representation. This is achieved by left-shifting the `r` value by 4, adding the `g` value
+     * left-shifted by 2, and adding the `b` value unchanged. The shifting is done in base 16.
+     */
+    toHex () {
+        return rgbToHex({ r: this.r, g: this.g, b: this.b });
+    }
+}
+
+
+export class HSV {
+    h = null;
+    s = null;
+    v = null;
+
+    constructor (h, s, v) {
+        this.h = h;
+        this.s = s;
+        this.v = v;
+    }
+
+    /**
+     * Converts the color to hexadecimal triplet notation, which is ThreeJS's preferred
+     * representation. This is achieved by left-shifting the `r` value by 4, adding the `g` value
+     * left-shifted by 2, and adding the `b` value unchanged. The shifting is done in base 16.
+     */
+    toHex () {
+        return rgbToHex(this.toRgb());
+    }
+
+    toRgb () {
+        const [r, g, b] = hsvToRgb(this.h, this.s, this.v);
+        return { r, g, b };
+    }
+}
