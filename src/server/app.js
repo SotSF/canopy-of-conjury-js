5538683da3357679f3e239770bfbd8ac605f2401
Milo Toor <mtoor@omicia.com>
2018-08-12 23:54:30 -0700

    Add index endpoint

--- src/server/app.js
+++ src/server/app.js
@@ -20,11 +20,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'src')));
-app.use('/lib', express.static(path.join(__dirname, 'lib')));
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 app.use('/api', routes);
+app.use('/dist', express.static(path.join(__dirname, '../../dist')));
+app.use('/lib', express.static(path.join(__dirname, '../../lib')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
