c2f8e31dfb5e7f216f234da0fae8bbed7fe2b538
Milo Toor <mtoor@omicia.com>
2018-08-13 14:35:45 -0700

    Create state-updating server endpoint

---
+++ src/server/state.ts
@@ -0,0 +1,81 @@
+/**
+ * The `/state` endpoint. This is a web socket endpoint that manages communication of state
+ * parameters between clients and the server
+ */
+
+import * as _ from 'lodash';
+import { allPatterns } from '../patterns';
+import { PatternInstance, PatternInterface } from '../types';
+import {
+    message,
+    AddPatternMessage,
+    RemovePatternMessage,
+    UpdatePropMessage,
+    MESSAGE_TYPE
+} from '../util/messaging';
+
+
+// The set of patterns that will be rendered
+const PATTERNS = {};
+
+/** Takes the display name of a pattern and returns the pattern class */
+const getPatternByName = (name: string) => {
+    for (let i = 0; i < allPatterns.length; i++) {
+        if (allPatterns[i].displayName === name) {
+            return allPatterns[i];
+        }
+    }
+
+    // Didn't find a pattern with the given name
+    return null;
+};
+
+/** Creates a unique ID for a pattern instance */
+const makePatternId = (): string => {
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    return _.range(20).map(() => _.sample(characters)).join('');
+};
+
+/** Adds a pattern to the set of active patterns */
+const addPattern = (msg: AddPatternMessage) => {
+    const PatternType: PatternInterface = getPatternByName(msg.patternName);
+    if (!PatternType) {
+        return null;
+    }
+
+    const newPattern = new PatternType();
+    newPattern.updateProps(msg.props);
+
+    const id = makePatternId();
+    PATTERNS[id] = newPattern;
+    return id;
+};
+
+/** Removes a pattern from the set of active patterns */
+const removePattern = (msg: RemovePatternMessage) => {
+    delete PATTERNS[msg.patternId];
+};
+
+/** Updates the property of an active pattern */
+const updateProp = (msg: UpdatePropMessage) => {
+    const pattern: PatternInstance = PATTERNS[msg.patternId];
+    pattern.updateProps({ [msg.prop]: msg.value });
+};
+
+export default (ws, req) => {
+    ws.on('message', (msg: message) => {
+        switch (msg.type) {
+            case MESSAGE_TYPE.addPattern:
+                addPattern(<AddPatternMessage>msg);
+                break;
+            case MESSAGE_TYPE.removePattern:
+                removePattern(<RemovePatternMessage>msg);
+                break;
+            case MESSAGE_TYPE.updateProp:
+                updateProp(<UpdatePropMessage>msg);
+                break;
+        }
+
+        // TODO: should clients be informed about the update?
+    });
+};
---
+++ src/util/messaging.ts
@@ -0,0 +1,35 @@
+
+/** Constants */
+export const enum MESSAGE_TYPE {
+    addPattern,
+    removePattern,
+    updateProp
+}
+
+
+/** Messages from the client */
+interface IMessage {
+    type: MESSAGE_TYPE
+}
+
+export interface AddPatternMessage extends IMessage {
+    patternName: string
+    props: any
+}
+
+export interface RemovePatternMessage extends IMessage {
+    patternId: string
+}
+
+export interface UpdatePropMessage extends IMessage {
+    patternId: string
+    prop: string
+    value: any
+}
+
+export type message = AddPatternMessage | RemovePatternMessage | UpdatePropMessage;
+
+
+/** Responses from the server */
+
+export type response = any;
