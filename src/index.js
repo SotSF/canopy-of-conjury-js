aa80250e69f212e55b0008ae9a6f450a78a3c470
Justyn Pham <jpham@extron.com>
2018-08-15 18:11:32 +0100

    Remove unused code (/brushes, /folders, patterns/gif)

--- src/index.js
+++ src/index.js
@@ -24,36 +24,7 @@ document.getElementById('renderer-wrapper').appendChild(renderer.domElement);
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 const canopy = new CanopyThreeJs;
 canopy.initialize(scene);
-
-var brush; // active freedraw brush
 var layers = [];
-var mapFromCanopyMemo = {};
-/*
-window.onload = function () {  
-    (function(){
-        let _mapFromCanopy = (s, l, numStrips) => {
-            let theta = s * 2 * Math.PI / numStrips;
-            let radius = l;
-            let x = parseInt(radius * Math.cos(theta) + Patterns.PCanvas.dimension / 2);
-            let y = parseInt(radius * Math.sin(theta) + Patterns.PCanvas.dimension / 2);
-            return {x,y};
-        }
-        canopy.strips.forEach((strip, s) => {
-            strip.leds.forEach((led, l) => {
-                mapFromCanopyMemo[s + "-" + l] = _mapFromCanopy(s, l, canopy.numStrips);
-            });
-        });
-    })();
-}
-const mapFromCanopy = (s, l) => { 
-    return mapFromCanopyMemo[s + "-" + l];
- }
-*/
-const clearCanopy = () => {
-    for (let s in canopy.strips) {
-        canopy.strips[s].updateColors("#000000");
-    }
-}
 
 /**
  * Iterates over the canopy's strips, pulling the colors from the LEDs in order to transmit them to
@@ -123,56 +94,4 @@ const updateLayers = function(val) {
 }
 $(document).ready(function () {
     Menu.initialize(updateLayers, setBrush, canopy);
-    //$(document).on('click', '#idRenderer', canopyClick);
 });
-
-var waitingOnTarget = false;
-var doubleBrush;
-const ray = new THREE.Raycaster();
-ray.params.Points.threshold = 0.5;
-/*
-function canopyClick( event ) {
-    if (brush) {
-        const pattern = null; // FIXME: re-enable brushing
-        const x = ((event.clientX - MENU_WIDTH) / (window.innerWidth - MENU_WIDTH) ) * 2 - 1;
-        const y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-        const vector = new THREE.Vector2( x, y );
-        ray.setFromCamera(vector, camera);
-        // create an array containing all objects in the scene with which the ray intersects
-        const intersects = ray.intersectObjects( canopy.ledHitBoxes );
-        intersects.sort((a,b) => { return (a.distanceToRay - b.distanceToRay)});
-        if ( intersects.length > 0 )
-        {
-            const intersect = intersects[0];
-            const s = canopy.ledHitBoxes.indexOf(intersect.object);
-            const l = intersect.index;
-            const coord = mapFromCanopy(s,l);
-
-            if (waitingOnTarget) {
-                waitingOnTarget = false;
-                doubleBrush.target = coord;
-                pattern.add(doubleBrush);
-                return;    
-            }
-
-            switch (brush) {
-                case "bubbles":
-                    pattern.add(new Brushes.BubbleBrush(Object.assign({}, Brushes.BubbleBrush.setParams), coord));
-                    break;
-                case "ring": 
-                    pattern.add(new Brushes.RingBrush(Object.assign({}, Brushes.RingBrush.setParams), coord));
-                    break;
-                case "radial":
-                    pattern.add(new Brushes.RadialBrush(Object.assign({}, Brushes.RadialBrush.setParams), coord));
-                    break;
-                case "line":
-                    waitingOnTarget = true;
-                    doubleBrush = new Brushes.LineBrush(Object.assign({}, Brushes.LineBrush.setParams), coord);
-                    break;
-            }
-            
-        }
-    }
-    
-}
-*/
