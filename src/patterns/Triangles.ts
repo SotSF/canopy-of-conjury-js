86c9d5d4b80324a931c13c907994408aa9445cdb
Justyn Pham <jpham@extron.com>
2018-08-15 16:35:47 -0700

    patterns/Triange - enable "inverted" prop

--- src/patterns/Triangles.ts
+++ src/patterns/Triangles.ts
@@ -57,8 +57,8 @@ export class Triangles extends BasePattern {
     }
 
     render(canopy) {
-        const radius : number = _.result(this.props, 'radius');
         const size : number = _.result(this.props, 'size');
+        const radius : number = _.result(this.props, 'radius');
         for (let i = 0 ; i < this.props.quantity; i++ ){
             let t = (this.indexOfBrightest - i);
             if (t < 0) t += this.props.quantity;
@@ -71,16 +71,20 @@ export class Triangles extends BasePattern {
                 color = this.props.color.withAlpha(this.props.opacity / 100 * triangleOpacity);
             }
 
-            const centerStrip = Math.floor(NUM_STRIPS / this.props.quantity * i);
-            const out = Math.floor(size / 2);
-            for (let s = -out; s <= out; s++) {
-                let strip = (centerStrip + s) % NUM_STRIPS;
-                if (strip < 0) strip += NUM_STRIPS;
-                for (let l = 0; l < size - Math.abs(s * 2); l++) {
-                    const led = Math.floor(radius + l);
-                    if (_.inRange(led, 0, NUM_LEDS_PER_STRIP)) {
-                    canopy.strips[strip].updateColor(led, color)
-                    }
+           this.renderTriangle(i, size, radius, canopy, color);
+        }
+    }
+
+    renderTriangle(i, size, radius, canopy, color) {  
+        const centerStrip = Math.floor(NUM_STRIPS / this.props.quantity * i);
+        const out = Math.floor(size / 2);
+        for (let s = -out; s <= out; s++) {
+            let strip = (centerStrip + s) % NUM_STRIPS;
+            if (strip < 0) strip += NUM_STRIPS;
+            for (let l = 0; l < size - Math.abs(s * 2); l++) {
+                const led = this.props.inverted ? Math.floor(radius - l) : Math.floor(radius + l);
+                if (_.inRange(led, 0, NUM_LEDS_PER_STRIP)) {
+                canopy.strips[strip].updateColor(led, color)
                 }
             }
         }
