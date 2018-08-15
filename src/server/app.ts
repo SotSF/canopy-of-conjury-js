ced2678021f04d1d01f28a5d4229701a7a376fa4
Milo Toor <mtoor@omicia.com>
2018-08-14 23:27:01 -0700

    Fix websocket connection

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,12 @@
 
-import * as fs from 'fs';
-import * as path from 'path';
+import * as bodyParser  from 'body-parser';
+import * as cookieParser  from 'cookie-parser';
 import * as express from 'express';
 import * as expressWs from 'express-ws';
+import * as fs from 'fs';
+import * as http from 'http';
 import * as logger from 'morgan';
-import * as cookieParser  from 'cookie-parser';
-import * as bodyParser  from 'body-parser';
+import * as path from 'path';
 
 import { Canopy } from '../canopy';
 import { PatternInstance } from '../types';
@@ -13,9 +14,8 @@ import state, { patterns } from './state';
 import Transmitter from './transmitter';
 
 
-// Set up the app object with web sockets
+/** Set up the app object */
 const app = express();
-expressWs(app);
 
 // A reference to the directory that the `app.ts` source file is found in. When the server code is
 // compiled, the executable code is moved into the `dist` directory and `app.ts` is located under
@@ -23,18 +23,17 @@ expressWs(app);
 // that `app.ts` is located in.
 const serverDirectory = path.join(__dirname, '../..');
 
-// view engine setup
+/** View engine setup */
 app.set('views', path.join(serverDirectory, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+/** Middleware */
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Static endpoints
+/** Static endpoints */
 app.use('/dist', express.static(path.join(serverDirectory, '../../dist')));
 app.use('/lib', express.static(path.join(serverDirectory, '../../lib')));
 app.use('/static', express.static(path.join(serverDirectory, '../static')));
@@ -42,32 +41,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(serverDirectory, '../static/index.html'));
 });
 
-// State endpoint
-app.ws('/state', state);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error('Not Found');
-    //err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
+/** Config reader */
 interface AppConfig {
     api_host: string
 }
 
-/** Config reader */
 const config: AppConfig = (() => {
     try {
         return JSON.parse(fs.readFileSync('./config.json', 'utf8'));
@@ -79,7 +58,7 @@ const config: AppConfig = (() => {
 })();
 
 
-// Create a connection to the API
+/** Create a connection to the API */
 const transmitter = new Transmitter(config.api_host);
 transmitter.ping().on('response', (resp) => {
     console.log('Connected to canopy API');
@@ -99,4 +78,33 @@ transmitter.ping().on('response', (resp) => {
     console.log('Unable to connect to canopy API');
 });
 
-module.exports = app;
+/** Create HTTP server. */
+const server = http.createServer(app);
+
+/** Add web sockets to the express app */
+expressWs(app, server);
+app.ws('/state', state);
+
+/** Error handling */
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    //err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+export {
+    app,
+    server
+};
