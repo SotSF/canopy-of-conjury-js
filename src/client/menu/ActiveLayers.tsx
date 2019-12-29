d7e93ac8df11525a34610611e5b74e18ba5e951a
Milo Toor <milo.toor@gmail.com>
2019-12-29 15:28:08 -0800

    Remove unused `SaveButton` component

--- src/client/menu/ActiveLayers.tsx
+++ src/client/menu/ActiveLayers.tsx
@@ -1,7 +1,5 @@
 
 import * as React from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
@@ -9,7 +7,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { withStyles, WithStyles, createStyles, Theme } from '@material-ui/core/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -52,22 +49,6 @@ const PatternStyles = createStyles({
         display: "inline-block"
     }
 });
-
-const SaveButtonStyles = (theme: Theme) => createStyles({
-    root: {
-        margin: `${theme.spacing()}px 0 ${theme.spacing()}px 16px`
-    },
-    card: {
-        backgroundColor: '#626262',
-        padding: '1rem',
-        display: 'flex',
-        alignItems: 'baseline'
-    },
-    saveButton: {
-        marginLeft: theme.spacing()
-    }
-});
-
 
 interface ActivePatternProps extends WithStyles<typeof PatternStyles> {
     pattern: PatternInstance
@@ -118,76 +99,6 @@ const ActivePattern = withStyles(PatternStyles)(
                     </Popover>
                 </ListItem>
             );
-        }
-    }
-);
-
-const SaveButton = withStyles(SaveButtonStyles)(
-    class SaveButton extends React.Component<WithStyles<typeof SaveButtonStyles>> {
-        state = {
-            anchorEl: null,
-            patternSetName: ''
-        };
-
-        handleClick = event => {
-            this.setState({ anchorEl: event.currentTarget });
-        };
-
-        handleClose = () => {
-            this.setState({ anchorEl: null });
-        };
-
-        savePatternSet = () => {
-            messenger.savePatternSet(this.state.patternSetName);
-        };
-        
-        updateName = (event) => {
-            this.setState({ patternSetName: event.target.value });
-        };
-
-        render () {
-            const { classes } = this.props;
-
-            // Can't save the pattern set if no name is entered
-            const disabled = this.state.patternSetName === '';
-
-            return [
-                <Button
-                    className={classes.root}
-                    color="primary"
-                    key="button"
-                    onClick={this.handleClick}
-                    size="small"
-                    variant="contained"
-                >
-                    Save Pattern Set
-                </Button>,
-                <Popover
-                    anchorEl={this.state.anchorEl}
-                    key="popover"
-                    onClose={this.handleClose}
-                >
-                    <Card className={classes.card}>
-                        <TextField
-                            id="pattern-set-name"
-                            label="Name"
-                            onChange={this.updateName}
-                            value={this.state.patternSetName}
-                        />
-
-                        <Button
-                            className={classes.saveButton}
-                            color="primary"
-                            disabled={disabled}
-                            onClick={this.savePatternSet}
-                            size="small"
-                            variant="contained"
-                        >
-                            Save
-                        </Button>
-                    </Card>
-                </Popover>
-            ];
         }
     }
 );
